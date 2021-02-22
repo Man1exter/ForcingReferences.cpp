70326487bed249c8c27818c16080bd299ff2b222
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-22 14:28:55 +0100

    basic includes/ file

---
+++ cerfref.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <iomanip>   //setw..
+#include <list>
+#include <forward_list>
+#include <typeinfo>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <typeinfo>
+#include <iterator>
+#include <set>
+#include <chrono>
+#include <thread>
+#include <future>
+#include "numbers.h"
+
+using namespace std;
+using namespace chrono;
+
+int main(){
+
+    return 0;
+}
